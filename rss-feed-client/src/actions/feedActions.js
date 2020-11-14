fa8028f7ebd3db84354ee44121d706fd8a957fde
Robert Ricci <rmricci121@gmail.com>
2020-11-14 06:14:19 -0500

    This commit adds an action to add a new feed

--- rss-feed-client/src/actions/feedActions.js
+++ rss-feed-client/src/actions/feedActions.js
@@ -5,3 +5,17 @@ export const fetchFeed = () => {
         .then(feed => dispatch({type: FETCH_FEED, payload:feed}))
     }
 }
+
+export const addFeed = (feed) => {
+    return dispatch => {
+        fetch("http://localhost:3000/feeds", {
+            method: 'POST', 
+            body: JSON.stringify(feed), 
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(resp => resp.json())
+        .then(feed => {
+            dispatch({type: 'ADD_FEED', payload:feed})
+        })
+    }
+}
---
+++ rss-feed-client/src/components/RssList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RssList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RssList;
