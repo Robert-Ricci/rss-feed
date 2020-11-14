bfdf0cb95e838f5c4d683b057c4a98187bd82c1c
Robert Ricci <rmricci121@gmail.com>
2020-11-14 06:16:34 -0500

    This commit adds the ADD_BLOG  reducer

--- rss-feed-client/src/reducers/feedReducer.js
+++ rss-feed-client/src/reducers/feedReducer.js
@@ -2,7 +2,10 @@ export const feedReducer = (state = [], action) => {
     switch(action.type) {
         case "FETCH_FEED": 
             return action.payload
+        case "ADD_FEED": 
+            return[...state, action.payload]
 
+            
             default: 
                 return state
     }
